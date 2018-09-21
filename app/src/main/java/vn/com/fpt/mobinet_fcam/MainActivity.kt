b38f051b498857ab76a4eda94fe0970b8983bb19
Pham Anh <phamanh1289@gmail.com>
2018-09-21 15:02:58 +0700

    - Init project & library

---
+++ app/src/main/java/vn/com/fpt/mobinet_fcam/MainActivity.kt
@@ -0,0 +1,12 @@
+package vn.com.fpt.mobinet_fcam
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
