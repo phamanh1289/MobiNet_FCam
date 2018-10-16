14694d6dc35caf257c040f0633f15c1cfe5098c3
Pham Anh <phamanh1289@gmail.com>
2018-10-16 16:13:39 +0700

    - Design UI : config nano

---
+++ app/src/main/java/vn/com/fpt/mobinet_fcam/data/network/api/ApiConfigOpenNet.kt
@@ -0,0 +1,99 @@
+package vn.com.fpt.mobinet_fcam.data.network.api
+
+import io.reactivex.Observable
+import okhttp3.ResponseBody
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import vn.com.fpt.mobinet_fcam.data.network.model.*
+
+/**
+ * *******************************************
+ * * Created by AnhPT76 on 26/09/2018.      **
+ * * Copyright (c)  by FPT Telecom          **
+ * * All rights reserved                    **
+ * *******************************************
+ */
+interface ApiService {
+    //=====POST=====
+    @POST("CheckIMEI")
+    fun checkIMEI(@Body map: HashMap<String, Any>): Observable<ResponseModel>
+
+    @POST("LoginV2")
+    fun postLogin(@Body map: HashMap<String, Any>): Observable<InfoUserModel>
+
+    @POST("GetInfoContractDepl")
+    fun getContractDeployment(@Body map: HashMap<String, Any>): Observable<SearchListContractModel>
+
+    @POST("GetInfoContractMain")
+    fun getContractMaintenance(@Body map: HashMap<String, Any>): Observable<SearchListContractModel>
+
+    @POST("GetListInfoContractDepl")
+    fun getListContractDepl(@Body map: HashMap<String, Any>): Observable<ResponseModel>
+
+    @POST("GetListInfoContractMain")
+    fun getListContractMain(@Body map: HashMap<String, Any>): Observable<ResponseModel>
+
+    @POST("GetDetailContractDepl")
+    fun getDetailContractDepl(@Body map: HashMap<String, Any>): Observable<DetailContractModel>
+
+    @POST("GetDetailContractMain")
+    fun getDetailContractMain(@Body map: HashMap<String, Any>): Observable<DetailContractModel>
+
+    @POST("UpdateDeploymentObjectV2")
+    fun postUpdateContractDeployment(@Body map: HashMap<String, Any>): Observable<ResponseModel>
+
+    @POST("UpdateMaintenanceObject")
+    fun postUpdateContractMaintenance(@Body map: HashMap<String, Any>): Observable<ResponseModel>
+
+    @POST("GetListMemberOfTeam")
+    fun getListMemberOfTeam(@Body map: HashMap<String, Any>): Observable<ResponseModel>
+
+    @POST("DivisionStaffMain")
+    fun postDivisionStaffMain(@Body map: HashMap<String, Any>): Observable<ResponseModel>
+
+    @POST("DivisionMember")
+    fun postDivisionMember(@Body map: HashMap<String, Any>): Observable<ResponseModel>
+
+    //=====GET=====
+    @GET("GetImage/{id}")
+    fun getImage(@Path("id") id: String): Observable<ResponseBody>
+
+    @GET("GetDeploymentObject/{userName}/{passWord}/{DeployID}/{ObjID}")
+    fun getDetailContract(@Path("userName") userName: String, @Path("passWord") passWord: String, @Path("DeployID") deplId: Int, @Path("ObjID") objId: Int): Observable<ResponseBody>
+
+    @GET("GetMaintenanceObject/{userName}/{passWord}/{MaintenanceID}/{ObjID}")
+    fun getMaintenanceObject(@Path("userName") userName: String, @Path("passWord") passWord: String, @Path("MaintenanceID") mainId: Int, @Path("ObjID") objId: Int): Observable<ResponseBody>
+
+    @GET("GetInfoContract/{contentSearch}/{typeFind}")
+    fun getInfoContract(@Path("contentSearch") userName: String, @Path("typeFind") passWord: String): Observable<SearchContractModel>
+
+    @GET("GetConnectionProperties/{objId}")
+    fun getConnectionProperties(@Path("objId") objId: String): Observable<ConnectPropertyModel>
+
+    @GET("GetKillSession/name/{userName}")
+    fun getKillSession(@Path("userName") objId: String): Observable<KillSessionModel>
+
+    @GET("KillSession/{objId}")
+    fun toKillSession(@Path("objId") objId: String): Observable<ResponseBody>
+
+    @GET("GetListConnection/{objId}")
+    fun getListConnection(@Path("objId") objId: String): Observable<Any>
+
+    @GET("GetMAC/1000/{objId}")
+    fun getMac(@Path("objId") objId: String): Observable<MacModel>
+
+    @GET("ResetMAC/{objId}/{newMac}")
+    fun resetMac(@Path("objId") objId: String,@Path("newMac") newMac: String): Observable<ResponseBody>
+
+    @GET("ResetPassword/{objId}/{newPassword}/{ipAddress}/0")
+    fun resetPassword(@Path("objId") objId: String,@Path("newPassword") newPassword: String,@Path("ipAddress") ipAddress: String): Observable<ResponseBody>
+
+    @GET("GetOnlineConnection/{objId}")
+    fun getOnlineConnection(@Path("objId") objId: String): Observable<Any>
+
+    @GET("GetLastAccessError/{objId}")
+    fun getLastAccessError(@Path("objId") objId: String): Observable<Any>
+
+}
---
+++ app/src/main/java/vn/com/fpt/mobinet_fcam/ui/utilities/config_ftth_nano/ConfigFtthNanoFragment.kt
@@ -0,0 +1,67 @@
+package vn.com.fpt.mobinet_fcam.ui.utilities.pico
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import vn.com.fpt.mobinet_fcam.R
+import vn.com.fpt.mobinet_fcam.data.network.model.SearchContractModel
+import vn.com.fpt.mobinet_fcam.data.network.model.TitleAndMenuModel
+import vn.com.fpt.mobinet_fcam.others.constant.Constants
+import vn.com.fpt.mobinet_fcam.ui.base.BaseFragment
+import vn.com.fpt.mobinet_fcam.utils.KeyboardUtils
+
+/**
+ * *******************************************
+ * * Created by AnhPT76 on 27/09/2018.      **
+ * * Copyright (c)  by FPT Telecom          **
+ * * All rights reserved                    **
+ * *******************************************
+ */
+class PicoFragment : BaseFragment(){
+//    @Inject
+//    lateinit var presenter: BlankPresenter
+private var searchContractModel: SearchContractModel? = null
+
+    companion object {
+        fun newInstance(model: SearchContractModel): PicoFragment {
+            val args = Bundle()
+            args.putParcelable(Constants.MODEL, model)
+            val fragment = PicoFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        getActivityComponent().inject(this)
+//        presenter.onAttach(this)
+        activity?.let { KeyboardUtils.setupUI(view, activity = it) }
+        initView()
+    }
+
+    private fun initView() {  setTitle(TitleAndMenuModel(title = getString(R.string.utilities_connection).replace("\n", " ")))
+        arguments?.let {
+            searchContractModel = it.getParcelable(Constants.MODEL) ?: SearchContractModel()
+        }
+    }
+
+//    override fun loadLogin(response: ResponseModel) {
+//
+//    }
+
+//    override fun handleError(response: String) {
+//        hideLoading()
+//        AppUtils.showDialog(fragmentManager, title = getString(R.string.mess_error_data), content = response, confirmDialogInterface = null)
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        presenter.onDetach()
+//    }
+}
